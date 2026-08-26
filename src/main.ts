aa743a097050fea840277d8ba1998c1884aec3e0
Marquis <abahmarquis@gmail.com>
2026-08-26 19:48:28 +0100

    fix: aggressively allow all CORS origins and headers

--- src/main.ts
+++ src/main.ts
@@ -5,24 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: (origin: string | undefined, callback: (err: Error | null, allow?: any) => void) => {
-      callback(null, origin || true);
-    },
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: [
-      'Origin',
-      'X-Requested-With',
-      'Content-Type',
-      'Accept',
-      'Authorization',
-      'Accept-Language',
-      'x-refresh-token',
-      'Cache-Control',
-      'Pragma',
-    ],
-    exposedHeaders: ['Authorization', 'x-refresh-token'],
     credentials: true,
-    maxAge: 86400,
   });
   const configService = app.get(ConfigService);
   const port = process.env.PORT || configService.get<number>('PORT') || 3002;
